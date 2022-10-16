2f580a127f209dc2bc8800a1bd95f119c33c3aa9
Varun <varunpatel207@gmail.com>
2022-10-16 19:54:31 -0400

    - make request to local

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const Task = require('../models/Task');
 
+const https = require('https');
+
+
+
 const router = express.Router();
 
 
 // get all tasks
 router.get('/', (req, res) => {
-    console.log("adding a task");
+    https.get('https://610e-174-95-129-74.ngrok.io/request', (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+
     res.send("adding a task");
+
+})
+
+
+// get all tasks
+router.get('/request', (req, res) => {
+    console.log("listening requests")
+    console.log(JSON.stringify(req.headers));
 })
 
 router.post('/add', (req, res) => {
